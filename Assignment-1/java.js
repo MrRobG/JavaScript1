fb9414061ae00c015fdba10f8891c9140c141eb3
MrRobG <robertguerra101@yahoo.com>
2023-03-07 12:38:59 -0700

    Update

--- Assignment-1/java.js
+++ Assignment-1/java.js
@@ -2,9 +2,9 @@ function myFunction() {
   let username = prompt('Please enter your name', 'Adventurer');
   if (username != null) {
     document.getElementById('demo').innerHTML =
-      'Hello ' + username + '! How are you today?';
+      'Hello Squire ' + username + '! Are you ready for an adventure!?';
   }
-  alert(' Hello ' + username + '. Welcome!');
+  alert(' Hello ' + username + '. Welcome to the quest we call javascript!');
 }
 
 console.log('Hello Adventurer!');
