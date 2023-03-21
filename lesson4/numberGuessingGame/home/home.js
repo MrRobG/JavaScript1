43c093a8e826739e880111ddf476f8410da6e1b9
MrRobG <robertguerra101@yahoo.com>
2023-03-20 19:25:19 -0600

    Number Game Update

--- lesson4/numberGuessingGame/home/home.js
+++ lesson4/numberGuessingGame/home/home.js
@@ -2,14 +2,11 @@ function submitData() {
   let min = guessForm.min.value;
   let max = guessForm.max.value;
   if (min == '' || max == '') {
-    updateDOM('Please enter a min and max range', 'white');
+    updateDOM('Please enter a min and max range', 'red');
     return false;
   }
-  if (min > max) {
-    updateDOM(
-      'The minimum range cannot be greater than the top range',
-      'white'
-    );
+  if (min.value > max.value) {
+    updateDOM('The minimum range cannot be greater than the top range', 'red');
     return false;
   }
   numberGuessGame(min, max);
@@ -20,10 +17,15 @@ function updateDOM(value, color) {
   document.getElementById('output').style.color = color;
 }
 function numberGuessGame(min, max) {
+  let number_of_guesses = 0;
   let num = Math.ceil(randomNumber(min, max));
   let guess;
-  let message = "I'm thinking of a number between" + min + ' and ' + max + '.';
-  (' Try to guess it!');
+  let message =
+    "I'm thinking of a number between " +
+    min +
+    ' and ' +
+    max +
+    '. Try to guess it!';
   console.log(num);
   do {
     if (guess < num) {
@@ -32,12 +34,15 @@ function numberGuessGame(min, max) {
     if (guess > num) {
       message = 'Too high try again!';
     }
+    number_of_guesses++;
     guess = parseInt(prompt(message));
   } while (num !== guess);
-
-  if ((guess = num)) {
-    guess = parseInt(alert('Congratulations! You have guessed correct!'));
-  }
+  updateDOM(
+    'Congratulations! You have guessed correct! It only took you ' +
+      number_of_guesses +
+      ' attempt(s)!',
+    'green'
+  );
 }
 function randomNumber(min, max) {
   return Math.random() * (max - min) + min;
